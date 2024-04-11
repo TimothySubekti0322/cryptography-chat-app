f1cd184180534abf3d1339daefb83f60a1cdaa75
TimothySubekti0322 <18221063@std.stei.itb.ac.id>
2024-04-11 15:22:32 +0700

    Init Repository

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const cors = require("cors");
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://10.0.2.2:3000/",
+  },
+});
+
+const PORT = 4000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+socketIO.on("connection", (socket) => {
+  console.log(`${socket.id} user is just connected`);
+
+  socket.on("push-message", (message) => {
+    console.log(`📩: ${message}`);
+    socket.emit("feedback", "Message received");
+  });
+});
+
+app.get("/api", (req, res) => {
+  res.json(chatgroups);
+});
+
+http.listen(PORT, () => {
+  console.log(`Server is listeing on ${PORT}`);
+});
